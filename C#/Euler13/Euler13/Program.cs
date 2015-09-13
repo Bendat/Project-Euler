6c738458746a86b1d93547c522c90a5f39a9cffb
Bendat <benaherne@outlook.ie>
2015-09-13 15:31:22 +0100

    Created language based folder hierarchy

---
+++ C#/Euler13/Euler13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler13
+{
+    class Program
+    {
+        public const string NumFile = "TextFile1.txt";
+        static void Main(string[] args)
+        {
+            if (!File.Exists(NumFile))
+            {
+                throw new NullReferenceException();
+            }
+            var numbers = File.ReadAllLines(NumFile).ToList();
+            string result = calculateSumOfList(numbers).ToString();
+            result = result.Substring(0, 10);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static BigInteger calculateSumOfList(List<string> list)
+        {
+            BigInteger num = new BigInteger();
+            num = 0;
+            foreach (string line in list)
+            {
+                num += BigInteger.Parse(line);
+            }
+            return num;
+        }
+    }
+}
