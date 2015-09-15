0a5e91b73f6f4fd798cb30b1c61e582068ce4c7b
Bendat <benaherne@outlook.ie>
2015-09-16 00:37:10 +0100

    Added solution 21 and .gitignore

---
+++ C#/Euler20/Euler_20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+/*
+n! means n × (n − 1) × ... × 3 × 2 × 1
+*For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+*and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+*Find the sum of the digits in the number 100!
+    */
+namespace Euler_20
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var num = factorial(100);
+            var sum = SumDigits(num);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        public static BigInteger factorial(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), "Input must be greater than 1");
+            }
+            if (number == 1)
+            {
+                return 1;
+            }
+            BigInteger num = new BigInteger(number);
+            return number*(factorial(number - 1));
+        }
+
+        public static BigInteger SumDigits(BigInteger num)
+        {
+            BigInteger sum = 0;
+            while (!num.Equals(0))
+            {
+                sum += (num%10);
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ C#/euler21/euler21/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+/*
+    Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+    If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+    For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+    Evaluate the sum of all the amicable numbers under 10000.
+*/
+namespace euler21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Dictionary<int, int> sumDictionary = new Dictionary<int, int>();
+            int totalSum = 0;
+            for (int i = 1; i < 10000; i++)
+            {
+                int sumOfNumber = GetProperDivisorsSum(i);
+                if (GetProperDivisorsSum(sumOfNumber) == i && i != sumOfNumber)
+                {
+                    totalSum += i;
+                }
+            }
+            Console.WriteLine(totalSum +" Run in: " + sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+
+        public static int GetProperDivisorsSum(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+      
+    }
+}
