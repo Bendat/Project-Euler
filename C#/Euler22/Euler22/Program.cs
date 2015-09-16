d4830ae16d6b9a1b764523f575523b74685678fc
Bendat <benaherne@outlook.ie>
2015-09-16 02:44:42 +0100

    Added Euler solution 23

---
+++ C#/Euler22/Euler22/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            string[] names = GetInput("p022_names.txt");
+            Quicksort(names, 0, names.Length - 1);
+            int sum = 0;
+            for (int i = 0; i < names.Length; i++)
+            {
+                sum += (Score(names[i]) * (i + 1));
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        private static int Score(string name)
+        {
+            name = name.ToUpper();
+            int sum = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                sum += (name[i] - 64);
+            }
+            return sum;
+        }
+
+        private static string[] GetInput(string filename)
+        {
+            string[] names;
+            if (!File.Exists(filename))
+            {
+                throw  new FileNotFoundException(nameof(filename));
+            }
+            names = File.ReadAllText(filename).Split(',');
+            for(int i = 0; i < names.Length; i++)
+            {
+                names[i] = names[i].Trim('"');
+
+            }
+            return names;
+        }
+
+        public static void Quicksort(string[] elements, int left, int right)
+        {
+            if (elements == null || elements.Length == 0)
+                return;
+            int leftPivot = left;
+            int rightPivot = right;
+            string pivot = elements[(left + right) / 2];
+
+            while (leftPivot <= rightPivot)
+            {
+                while (String.Compare(elements[leftPivot], pivot, StringComparison.Ordinal) < 0)
+                {
+                    leftPivot++;
+                }
+
+                while (String.Compare(elements[rightPivot], pivot, StringComparison.Ordinal) > 0)
+                {
+                    rightPivot--;
+                }
+
+                if (leftPivot <= rightPivot)
+                {
+                    string tmp = elements[leftPivot];
+                    elements[leftPivot] = elements[rightPivot];
+                    elements[rightPivot] = tmp;
+
+                    leftPivot++;
+                    rightPivot--;
+                }
+            }
+            if (left < rightPivot)
+            {
+                Quicksort(elements, left, rightPivot);
+            }
+
+            if (leftPivot < right)
+            {
+                Quicksort(elements, leftPivot, right);
+            }
+        }
+    }
+}
---
+++ C#/euler23/euler23/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler23
+{
+    class Program
+    {
+        const int Max = 28123;
+        private static readonly bool[] IsAbundent = new bool[Max+1];
+
+        static void Main(string[] args)
+        {
+            AddAbundentsToArray();
+            int sum = SumNoneAbundentNumbers();
+            Console.WriteLine(sum + " ");
+            Console.ReadKey();
+        }
+
+        private static int SumNoneAbundentNumbers()
+        {
+            int sum = 0;
+            for (int i = 1; i < Max; i++)
+            {
+                if (!IsSumOfAbundent(i))
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        private static void AddAbundentsToArray()
+        {
+            for (int i = 1; i < IsAbundent.Count(); i++)
+            {
+                IsAbundent[i] = IsAbundant(i);
+            }
+        }
+
+        private static bool IsSumOfAbundent(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if(IsAbundent[i] && IsAbundent[n - i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool IsAbundant(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum > num;
+        }
+    }
+}
